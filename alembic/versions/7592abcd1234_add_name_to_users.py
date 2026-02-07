40176d71f151324d7f1bebc69477cdbfad18e693
pranith-akula <neoapeclub1@gmail.com>
2026-02-07 20:22:26 +0530

    feat: Add DB migration for user name column

---
+++ alembic/versions/7592abcd1234_add_name_to_users.py
@@ -0,0 +1,24 @@
+"""add_name_to_users
+
+Revision ID: 7592abcd1234
+Revises: 62746616708a
+Create Date: 2026-02-07 20:35:00.000000
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7592abcd1234'
+down_revision = '62746616708a'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.add_column('users', sa.Column('name', sa.String(length=255), nullable=True))
+
+
+def downgrade() -> None:
+    op.drop_column('users', 'name')
